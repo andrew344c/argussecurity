2fb582ed98fb30be7868a7d58b5062fb50fa820a
andrew344c <32402997+andrew344c@users.noreply.github.com>
2020-02-23 09:15:33 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,49 @@
+function loginAuth() {
+    window.location.replace("account.html");
+}
+
+function initMap() {
+    var location = {
+        lat: 37.787731,
+        lng: -122.411018
+    };
+    var map = new google.maps.Map(
+        document.getElementById('gun-map'), {
+            zoom: 9,
+            center: location
+        });
+
+    var marker = new google.maps.Marker({
+        position: {
+            lat: 37.3006,
+            lng: -122.0046
+        },
+        map: map
+            // icon: 'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png'
+
+    });
+}
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyCiz2IUpuVqPo7M45ayzsOucu7Ftzeughw",
+    authDomain: "dvhacks-9ace0.firebaseapp.com",
+    databaseURL: "https://dvhacks-9ace0.firebaseio.com",
+    projectId: "dvhacks-9ace0",
+    storageBucket: "dvhacks-9ace0.appspot.com",
+    messagingSenderId: "403365043007",
+    appId: "1:403365043007:web:8c0b55eb63ac47a4aa518a",
+    measurementId: "G-K1K9WRSJ71"
+};
+// var firebase = require('firebase/database');
+// // Initialize Firebase
+// firebase.initializeApp(firebaseConfig);
+// firebase.analytics();
+
+// var database = firebase.database();
+// var ref = database.ref('number');
+
+var data = {
+        number: 12
+    }
+    // ref.push(data);
